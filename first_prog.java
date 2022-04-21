fb16aa8a7f10f965e2026765174c92c3eb49283f
Jenin08 <93582445+Jenin08@users.noreply.github.com>
2022-04-21 17:37:03 +0530

    Add files via upload

---
+++ first_prog.java
@@ -0,0 +1,9 @@
+	class first_prog
+
+	{
+		public static void main(String args[])
+		
+		{
+			System.out.println("hi jenin");
+		}
+	}
---
+++ pro3.java
@@ -0,0 +1,11 @@
+import java.util. *;
+class pro3
+{
+	public static void main(String args[])
+	{
+		int num1,num2,res;
+		num1 = 70; num2 = 66;
+		res = num1+num2;
+		System.out.println("the answer of two no.s = "+res);
+	}
+}
